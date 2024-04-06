ef519895c98e8a5bb799b26bb2b0e96517decc8f
Saksham Arora <isakshamarora@gmail.com>
2024-04-06 12:50:04 +0530

    update until the end

--- testcase.py
+++ testcase.py
@@ -1,13 +1,20 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Start the WebDriver and open the HTML page
+service = Service(executable_path='/usr/local/bin/chromedriver')
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(service=service, options=options)
+
 # URL of your webpage
 URL = 'https://sakshamarora2002.github.io/MagicWebsite-/'
-
-# Initialize Chrome WebDriver
-driver = webdriver.Chrome()
 
 try:
     # Open the webpage
